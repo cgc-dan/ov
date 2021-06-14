09a8b6a933e88efe9585dbe421d9e62730fbd658
Dan N <13005368+cssmfc@users.noreply.github.com>
2021-06-14 21:26:43 +0300

    Update imgcgc.php

--- prototype/imgcgc.php
+++ prototype/imgcgc.php
@@ -1,25 +1,96 @@
 <?php
-// Print two names on the picture, which accepted by query string parameters.
+// Specify font path
+$font = 'fonts/verdana.ttf';
 
-$n1 = $_GET['n1'];
-$n2 = $_GET['n2'];
+// Text font size
+$font_size = 10;
 
-Header ("Content-type: image/jpeg");
-$image = imageCreateFromJPEG("images/wider.jpg");
-$color = ImageColorAllocate($image, 255, 255, 255);
+// Get settings from URL
+$setting = isset($_GET['s']) ? $_GET['s'] : "000_FFF_350_350";
+$setting = explode("_", $setting);
 
-// Calculate horizontal alignment for the names.
-$BoundingBox1 = imagettfbbox(13, 0, 'ITCKRIST.TTF', $n1);
-$boyX = ceil((125 - $BoundingBox1[2]) / 2); // lower left X coordinate for text
-$BoundingBox2 = imagettfbbox(13, 0, 'ITCKRIST.TTF', $n2);
-$girlX = ceil((107 - $BoundingBox2[2]) / 2); // lower left X coordinate for text
+$img = array();
 
-// Write names.
-imagettftext($image, 13, 0, $boyX+25, 92, $color, 'ITCKRIST.TTF', $n1);
-imagettftext($image, 13, 0, $girlX+310, 92, $color, 'ITCKRIST.TTF', $n2);
+// Define image width, height, and color
+switch($n = count($setting)){
+    case $n > 4 :
+    case 3:
+        $setting[3] = $setting[2];
+    case 4:
+        $img['width'] = (int) $setting[2];
+        $img['height'] = (int) $setting[3];
+    case 2:
+        $img['background'] = $setting[0];
+        $img['color'] = $setting[1];
+        break;
+    default:
+        list($img['background'], $img['color'], $img['width'], $img['height']) = array('F', '0', 100, 100);
+        break;
+}
+$background = explode(",",hex2rgb($img['background']));
+$textColorRgb = explode(",",hex2rgb($img['color']));
+$width = empty($img['width']) ? 100 : $img['width'];
+$height = empty($img['height']) ? 100 : $img['height'];
 
-// Return output.
-ImageJPEG($image, NULL, 93);
-ImageDestroy($image);
+// Get text from URL
+$text = (string) isset($_GET['t']) ? urldecode($_GET['t']) : $width ." x ". $height;
+
+// Create the image resource 
+$image = @imagecreate($width, $height) or die("Cannot Initialize new GD image stream");
+
+// Create image background
+$background_color = imagecolorallocate($image, $background[0], $background[1], $background[2]);
+
+// Grab the width & height of the text box
+$bounding_box_size = imagettfbbox($font_size, 0, $font, $text);
+$text_width = $bounding_box_size[2] - $bounding_box_size[0];
+$text_height = $bounding_box_size[7]-$bounding_box_size[1];
+
+// Text x&y coordinates
+$x = ceil(($width - $text_width) / 2);
+$y = ceil(($height - $text_height) / 2);
+
+// Define text color
+$text_color = imagecolorallocate($image, $textColorRgb[0], $textColorRgb[1], $textColorRgb[2]);
+
+// Write text to image
+imagettftext($image, $font_size, 0, $x, $y, $text_color, $font, $text);
+
+// Set the content type header - in this case image/png
+header('Content-Type: image/png');
+
+// Output the image
+imagepng($image);
+
+// Free up memory
+imagedestroy($image);
+
+// Convert color code to rgb
+function hex2rgb($hex) {
+    $hex = str_replace("#", "", $hex);
+
+    switch(strlen($hex)){
+        case 1:
+            $hex = $hex.$hex;
+        case 2:
+            $r = hexdec($hex);
+            $g = hexdec($hex);
+            $b = hexdec($hex);
+            break;
+        case 3:
+            $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+            $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+            $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+            break;
+        default:
+            $r = hexdec(substr($hex,0,2));
+            $g = hexdec(substr($hex,2,2));
+            $b = hexdec(substr($hex,4,2));
+            break;
+    }
+
+    $rgb = array($r, $g, $b);
+    return implode(",", $rgb); 
+}
 ?>
-<img src="imgcgc.php?n1=bebe&n2=jake" />
+<img src="imgcgc.php?s=000_FFF_350_350&t=Dynamic%20Image">
